86a1de0724d7616d447a71c3313d41a01b66103a
seungjun6750 <haseungjun366@gmail.com>
2023-07-08 17:59:03 +0900

    v1

--- script.js
+++ script.js
@@ -1,72 +1,71 @@
-document.addEventListener("DOMContentLoaded", 
-    function(e){
-        var configID = document.querySelector("#id i")
-        var idText = document.querySelector("#id span")
+document.addEventListener("DOMContentLoaded",
+function(e){
+    let configID = document.querySelector("#id i")
+    let idText = document.querySelector("#id span")
 
-        configID.addEventListener("click",
-            function(e){
-                idText.textContent = prompt("새로운 아이디를 입력하세요")
-            }
-        )
-        
-        var profileEditButton = document.querySelector("#profile_info button")
-        var userInfo = document.querySelector("#userInfo")
-        var summary = document.querySelector("#summary") 
-        var profileDetail = document.querySelector("#profileDetail") 
-        var changing = false
+    configID.addEventListener("click",
+    function (e) {
+            idText.textContent = prompt("새로운 아이디를 입력하세요")
+        }
+)
 
-        profileEditButton.addEventListener("click",
-            function (e) {
-                if (changing) {
-                    var _userInfo = userInfo.querySelector("input").value
-                    var _summary = summary.querySelector("input").value
-                    var _profileDetail = profileDetail.querySelector("input").value
+let profileEditButton = document.querySelector("#profile_info button")
+let userInfo = document.querySelector("#userInfo")
+let summary = document.querySelector("#summary") 
+let profileDetail = document.querySelector("#profileDetail") 
+let changing = false
 
-                    userInfo.innerHTML = _userInfo
-                    summary.innerHTML = _summary
+profileEditButton.addEventListener("click",
+    function (e) {
+        if (changing) {
+            let _userInfo = userInfo.querySelector("input").value
+            let _summary = summary.querySelector("input").value
+            let _profileDetail = profileDetail.querySelector("input").value
 
-                    if (_profileDetail.startsWith("http")){
-                        _profileDetail = "<a href=" + _profileDetail + ">" + _profileDetail + "</a>"
-                    } 
+            userInfo.innerHTML = _userInfo
+            summary.innerHTML = _summary
 
-                    profileDetail.innerHTML = _profileDetail
-                    
-                    e.target.textContent = "프로필 편집"
-                    changing = false
+            if (_profileDetail.startsWith("http")){
+                _profileDetail = "<a href=" + _profileDetail + ">" + _profileDetail + "</a>"
+            } 
 
-                } else {
-                    var _userInfo = userInfo.textContent
-                    var _summary = summary.textContent
-                    var _profileDetail = profileDetail.textContent
+            profileDetail.innerHTML = _profileDetail
+            
+            e.target.textContent = "프로필 편집"
+            changing = false
 
-                    userInfo.innerHTML = "<input value=" + _userInfo + "></input>"
-                    summary.innerHTML = "<input value=" + _summary + "></input>"
-                    profileDetail.innerHTML = "<input value=" + _profileDetail + "></input>"
-                    
-                    e.target.textContent = "프로필 편집 완료"
-                    changing  =true
-                }
-            }
-        )
+        } else {
+            let _userInfo = userInfo.textContent
+            let _summary = summary.textContent
+            let _profileDetail = profileDetail.textContent
 
-        var profile_pic = document.querySelector("#profile_pic .circle_pic")
-        profile_pic.addEventListener("mouseover",
-            function (e){
-                e.target.style.filter = "grayscale(50%)"
-            }
-        )
-        
-        profile_pic.addEventListener("mouseleave",
-            function (e){
-                e.target.style.filter = "grayscale(0%)"
-            }
-        )
-        
-        profile_pic.addEventListener("click",
-            function (e){
-                profile_pic.setAttribute("src", prompt("이미지 url을 입력해 주세요!"))
-            }
-        )
+            userInfo.innerHTML = "<input value=" + _userInfo + "></input>"
+            summary.innerHTML = "<input value=" + _summary + "></input>"
+            profileDetail.innerHTML = "<input value=" + _profileDetail + "></input>"
+            
+            e.target.textContent = "프로필 편집 완료"
+            changing  =true
+        }
     }
 )
 
+let profile_pic = document.querySelector("#profile_pic .circle_pic")
+profile_pic.addEventListener("mouseover",
+    function (e){
+        e.target.style.filter = "grayscale(50%)"
+    }
+)
+
+profile_pic.addEventListener("mouseleave",
+    function (e){
+        e.target.style.filter = "grayscale(0%)"
+    }
+)
+
+profile_pic.addEventListener("click",
+    function (e){
+        profile_pic.setAttribute("src", prompt("이미지 url을 입력해 주세요!"))
+    }
+)
+}
+)
